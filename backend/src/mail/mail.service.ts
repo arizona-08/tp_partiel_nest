5f942d3434a0475ee4055e2618e87cc8b61d3fcb
Jonathan Assi <assijonathan2@gmail.com>
2026-04-27 15:11:01 +0200

    structure de base pour l'envoie de mail

--- backend/src/mail/mail.service.ts
+++ backend/src/mail/mail.service.ts
@@ -1,12 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class MailService {
+  private readonly logger = new Logger(MailService.name);
+
+  constructor(private readonly mailerService: MailerService) {}
+
   async sendVerificationEmail(email: string, code: string) {
-    console.log(`mail de verification envoyé à ${email} avec le code : ${code}`);
+    try {
+      await this.mailerService.sendMail({
+        to: email, // Adresse de destination
+        subject: 'Vérification de votre compte', // Objet
+        text: `Voici votre code de vérification : ${code}`, // Version texte
+        html: `<p>Voici votre code de vérification : <b>${code}</b></p>`, // Version HTML
+      });
+      this.logger.log(`Email de vérification envoyé à ${email}`);
+    } catch (error) {
+      this.logger.error("Erreur lors de l'envoi de l'email de vérification", error);
+    }
   }
 
   async sendTwoFactorCode(email: string, code: string) {
-    console.log(`mail de verification 2FA envoyé à ${email} avec le code : ${code}`);
+    try {
+      await this.mailerService.sendMail({
+        to: email,
+        subject: 'Votre code 2FA',
+        text: `Votre code d'authentification à deux facteurs est : ${code}`,
+        html: `<p>Votre code d'authentification à deux facteurs est : <b>${code}</b></p>`,
+      });
+      this.logger.log(`Email 2FA envoyé à ${email}`);
+    } catch (error) {
+      this.logger.error("Erreur lors de l'envoi de l'email 2FA", error);
+    }
   }
 }
